9857d1dcbeb015e3bef5479c817f9990b8ba913b
longwinpro113 <62163976+longwinpro113@users.noreply.github.com>
2025-03-08 09:18:09 +0000

    add public file

--- src/server.js
+++ src/server.js
@@ -8,7 +8,14 @@ const port = process.env.PORT || 3000
 app.set('views', path.join(__dirname, 'views')) 
 app.set('view engine', 'ejs')
 
+//todo config static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', function (req, res) {
+  res.send('Hello Long')
+})
+
+app.get('/long', function (req, res) {
   res.render('sample.ejs')
 })
 
