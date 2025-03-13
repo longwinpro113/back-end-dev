22d74664720cfeb49e8f93f0639c7bfc23643a2b
Long Nguyen <longwinpro113@gmail.com>
2025-03-13 14:56:43 +0700

    add form manage users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,37 @@
 const { json } = require('express');
 const connection = require('../config/database')
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Create a connection pool to the database
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 const getHomePage = (req, res) => {
-  return res.render('homepage.ejs')
-}
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            res.status(500).send('Error connecting to the database');
+            return;
+        }
+        console.log('Connected to the MySQL database.');
+        connection.release();
+        return res.render('homepage.ejs');
+    });
+};
 
 const getProduct = (req, res) => {
-  res.render('sample.ejs')
-}
+    res.render('sample.ejs');
+};
 
 module.exports = {
-  getHomePage, getProduct
-}
+    getHomePage,
+    getProduct
+};
