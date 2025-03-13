b2283b9b2c27c10e00e01a98fe08bd7226087738
Long Nguyen <longwinpro113@gmail.com>
2025-03-13 08:30:46 +0700

    Update server.js to use environment variables for MySQL connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
+const mysql = require('mysql2')
 const webRoutes = require('./routes/web')
 
 const app = express()
@@ -12,5 +13,26 @@ configViewEngine(app)
 
 //? Routes Declaration
 app.use('/', webRoutes)
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack)
+        return
+    }
+    console.log('Connected to the database.')
+
+    // Query to select all records from the users table
+    connection.query('SELECT * FROM users', (error, results, fields) => {
+        if (error) throw error
+        console.log('Users:', results)
+    })
+})
 
 app.listen(port)
